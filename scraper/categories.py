9c00587b41eb9b153abd9b411ebf3c860aa53859
unknown <ge.georgiev95@gmail.com>
2025-07-09 18:57:53 +0300

    Collect the required category for 3 grocery stores

--- scraper/categories.py
+++ scraper/categories.py
@@ -1,17 +1,288 @@
-CATEGORIES = [
-    {
-        "url": "https://glovoapp.com/bg/en/sofia/kaufland-sof?content=yaytsa-c.22267632&section=kokoshi-yaytsa-s.22267920",
-        "name": "Яйца",
-        "scrape_limit": 10
-    },
-    {
-        "url": "https://glovoapp.com/bg/en/sofia/kaufland-sof?content=kiseli-mleka-c.22267852&section=krave-kiselo-mlyako-s.22267683",
-        "name": "Кисело мляко",
-        "scrape_limit": 10
-    },
-    {
-        "url": "https://glovoapp.com/bg/en/sofia/kaufland-sof?content=presni-i-uht-mleka-c.22267901&section=pryasno-mlyako-s.22267863",
-        "name": "Прясно мляко",
-        "scrape_limit": 10
-    },
-]
+CATEGORIES= {
+    "kaufland_sofia": [
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/kaufland-sof?content=yaytsa-c.22267632&section=kokoshi-yaytsa-s.22267920",
+            "name": "Яйца",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/kaufland-sof?content=kiseli-mleka-c.22267852&section=krave-kiselo-mlyako-s.22267683",
+            "name": "Кисело мляко",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/kaufland-sof?content=presni-i-uht-mleka-c.22267901&section=pryasno-mlyako-s.22267863",
+            "name": "Прясно мляко",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/kaufland-sof?content=variva-c.22262504&section=oriz-s.22262609",
+            "name": "Ориз",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/kaufland-sof?content=osnovni-hrani-i-konservi-sc.22262334%2Fbrashno-c.22262563",
+            "name": "Брашно",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/kaufland-sof?content=hlyab-i-testeni-izdeliya-sc.22262369%2Fpaketiran-hlyab-c.22262394",
+            "name": "Хляб",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/kaufland-sof?content=meso-i-riba-sc.22262226%2Fkayma-c.22262405",
+            "name": "Кайма",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/kaufland-sof?content=yaytsa-i-mlechni-produkti-sc.22262246%2Fsirena-c.22262533",
+            "name": "Сирене",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/kaufland-sof?content=yaytsa-i-mlechni-produkti-sc.22262246%2Fkashkaval-c.22262502",
+            "name": "Кашкавал",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/kaufland-sof?content=yaytsa-i-mlechni-produkti-sc.22262246%2Fmaslo-i-margarin-c.22262457",
+            "name": "Масло",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/kaufland-sof?content=maslo-i-margarin-c.22262457&section=margarin-s.22262556",
+            "name": "Маргарин",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/kaufland-sof?content=olio-otset-i-rastitelni-masla-c.22262589&section=olio-s.22262590",
+            "name": "Олио",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/kaufland-sof?content=variva-c.22262504&section=bob-s.22262480",
+            "name": "Боб",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/kaufland-sof?content=variva-c.22262504&section=leshcha-s.22262591",
+            "name": "Леща",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/kaufland-sof?content=osnovni-hrani-i-konservi-sc.22262334%2Fzahar-i-sol-c.22262588",
+            "name": "Захар",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/kaufland-sof?content=shokoladi-c.22262677&section=mlechen-shokolad-s.22262561",
+            "name": "Шоколад",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/kaufland-sof?content=olio-otset-i-rastitelni-masla-c.22262589&section=otset-s.22262465",
+            "name": "Оцет",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/kaufland-sof?content=zahar-i-sol-c.22262588&section=sol-s.22262564",
+            "name": "Сол",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/kaufland-sof?content=voda-i-bezalkoholni-napitki-sc.22262300%2Fkafe-c.22262390",
+            "name": "Кафе",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/kaufland-sof?content=gazirani-napitki-c.22262281&section=kola-s.22262420",
+            "name": "Газирани напитки",
+            "scrape_limit": 10
+        }
+    ],
+    "billa_sofia": [
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/billa-sof1?content=mlechni-produkti-i-yaytsa-sc.8177445%2Fyaytsa-c.8177431",
+            "name": "Яйца",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/billa-sof1?content=kiselo-i-pryasno-mlyako-c.8177427&section=kiselo-mlyako-s.15457768",
+            "name": "Кисело мляко",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/billa-sof1?content=kiselo-i-pryasno-mlyako-c.8177427&section=pryasno-mlyako-s.13557378",
+            "name": "Прясно мляко",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/billa-sof1?content=variva-c.8177368&section=oriz-s.8177366",
+            "name": "Ориз",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/billa-sof1?content=zahar-sol-i-brashno-c.8177378&section=brashno-s.8177375",
+            "name": "Брашно",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/billa-sof1?content=billa-pekarna-sc.8177505%2Fpaketiran-hlyab-c.8177491",
+            "name": "Хляб",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/billa-sof1?content=meso-riba-i-kolbasi-sc.8177487%2Fkayma-i-mesni-zagotovki-c.8177440",
+            "name": "Кайма и месни заготовки",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/billa-sof1?content=mlechni-produkti-i-yaytsa-sc.8177445%2Fsirene-i-izvara-c.8177435",
+            "name": "Сирене",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/billa-sof1?content=mlechni-produkti-i-yaytsa-sc.8177445%2Fkashkaval-c.8177429",
+            "name": "Кашкавал",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/billa-sof1?content=mlechni-produkti-i-yaytsa-sc.8177445%2Fmaslo-smetana-i-margarin-c.8177433",
+            "name": "Масло и маргарин",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/billa-sof1?content=maslinovi-i-rastitelni-masla-olio-i-otset-c.8177391&section=olio-s.8177390",
+            "name": "Олио",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/billa-sof1?content=variva-c.8177368&section=bob-s.8177365",
+            "name": "Боб",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/billa-sof1?content=variva-c.8177368&section=leshcha-s.8177367",
+            "name": "Леща",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/billa-sof1?content=zahar-sol-i-brashno-c.8177378&section=zahar-s.8177377",
+            "name": "Захар",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/billa-sof1?content=zaharni-izdeliya-sc.8177346%2Fshokoladi-c.8177328",
+            "name": "Шоколад",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/billa-sof1?content=maslinovi-i-rastitelni-masla-olio-i-otset-c.8177391&section=otset-s.8177388",
+            "name": "Оцет",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/billa-sof1?content=zahar-sol-i-brashno-c.8177378&section=sol-s.8177376",
+            "name": "Сол",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/billa-sof1?content=kafe-i-chay-c.8177459&section=kafe-na-zrna-i-mlyano-s.14303366",
+            "name": "Кафе",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/billa-sof1?content=napitki-sc.8177494%2Fbezalkoholni-napitki-c.8177481",
+            "name": "Газирани напитки",
+            "scrape_limit": 10
+        }
+    ],
+    "fantastico_sofia": [
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/coca-cola-real-magic-sof?content=mlechni-produkti-i-yaytsa-sc.12810899%2Fyaytsa-c.12810915",
+            "name": "Яйца",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/coca-cola-real-magic-sof?content=mlechni-produkti-c.12810891&section=kiselo-mlyako-s.12810890",
+            "name": "Кисело мляко",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/coca-cola-real-magic-sof?content=mlechni-produkti-c.12810891&section=pryasno-mlyako-s.12810889",
+            "name": "Прясно мляко",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/coca-cola-real-magic-sof?content=osnovni-stoki-sc.12810892%2Foriz-i-zrneno-bobovi-c.12810874",
+            "name": "Ориз и зърнено бобови",
+            "scrape_limit": 50
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/coca-cola-real-magic-sof?content=osnovni-stoki-sc.12810892%2Fbrashno-c.12810879",
+            "name": "Брашно",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/coca-cola-real-magic-sof?content=hlyab-i-hlebni-izdeliya-c.12810919&section=hlyab-s.12810911",
+            "name": "Хляб",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/coca-cola-real-magic-sof?content=meso-i-riba-sc.12810920%2Fmlyano-meso-c.12810896",
+            "name": "Кайма и месни заготовки",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/coca-cola-real-magic-sof?content=sirene-i-kashkaval-c.12810884&section=sirena-s.12810882",
+            "name": "Сирене",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/coca-cola-real-magic-sof?content=sirene-i-kashkaval-c.12810884&section=kashkaval-s.12810881",
+            "name": "Кашкавал",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/coca-cola-real-magic-sof?content=mlechni-produkti-c.12810891&section=maslo-s.12810886",
+            "name": "Масло",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/coca-cola-real-magic-sof?content=olio-i-rastitelni-masla-c.12810872&section=olio-s.12810871",
+            "name": "Олио",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/coca-cola-real-magic-sof?content=sol-i-zahar-c.12810877&section=zahar-s.12810876",
+            "name": "Захар",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/coca-cola-real-magic-sof?content=zaharni-izdeliya-sc.12811551%2Fshokoladi-c.12811540",
+            "name": "Шоколад",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/coca-cola-real-magic-sof?content=osnovni-stoki-sc.12810892%2Fotset-i-balsamov-otset-c.12810869",
+            "name": "Оцет",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/coca-cola-real-magic-sof?content=sol-i-zahar-c.12810877&section=sol-s.12810875",
+            "name": "Сол",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/coca-cola-real-magic-sof?content=kafe-i-topli-napitki-c.12812577&section=mlyano-kafe-s.12812573",
+            "name": "Кафе",
+            "scrape_limit": 10
+        },
+        {
+            "url": "https://glovoapp.com/bg/en/sofia/coca-cola-real-magic-sof?content=bezalkoholni-napitki-c.12812567&section=gazirani-napitki-s.12812566",
+            "name": "Газирани напитки",
+            "scrape_limit": 10
+        }
+    ]
+}
